a63608ceb63acf68fda78828dda54fb357ae4ce4
Susumu Hoshikawa <hoshi005@me.com>
2024-01-14 12:56:31 +0900

    iOS向けのウィジェットを追加

---
+++ ios/HomeWidgetSample/HomeWidgetSampleBundle.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWidgetSampleBundle.swift
+//  HomeWidgetSample
+//
+//  Created by Susumu Hoshikawa on 2024/01/14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomeWidgetSampleBundle: WidgetBundle {
+    var body: some Widget {
+        HomeWidgetSample()
+        HomeWidgetSampleLiveActivity()
+    }
+}
